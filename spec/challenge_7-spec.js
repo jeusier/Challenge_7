97dadee4ad7c06b177694d6c7297860e7547b7c6
jahsan87 <jahsan87@gmail.com>
2014-03-05 09:21:21 -1000

    added failing tests

--- spec/challenge_7-spec.js
+++ spec/challenge_7-spec.js
@@ -1,5 +1,19 @@
 // import challenge 7
+var LastPrime = require("../challenge_7").LastPrime;
 
 // describe the class PrimeFinder
+describe("The last prime number", function () {
 
 // write your tests here
+
+    it("for the first 10 natural numbers is XXX", function () {
+        var primeLast = new LastPrime(10);
+        expect( primeLast.lastPrimeNumber() ).toEqual(7);
+    });
+
+    it("for the first 10,001 natural numbers is XXX", function () {
+        var primeLast = new LastPrime(10001);
+        expect( primeLast.lastPrimeNumber() ).toEqual(-1);
+    });
+
+});
