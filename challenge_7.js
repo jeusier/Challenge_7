358e1c04ac29d264642586a9f17a2ab0bc661de0
jahsan87 <jahsan87@gmail.com>
2014-03-05 09:55:21 -1000

    completed last prime number up to n

--- challenge_7.js
+++ challenge_7.js
@@ -1,1 +1,40 @@
+/*  PrimeFinder(n)
+ *
+ *  @param n        the nth prime number to find
+ *
+ *  @returns the nth prime number
+ */
+
 // do work here
+
+exports.PrimeFinder = PrimeFinder;
+
+function PrimeFinder(n){
+  this.n = n;
+
+  this.getPrimeNumber = function() {
+    var prime = 0;
+    var isPrime = false;
+    console.log(isPrime);
+    for (var i = 2; i < this.n; i++) {
+        for (var j = 2; j < i; j++) {
+            if (i%j !== 0){
+                isPrime = true;
+            } else {
+                isPrime = false;
+                break;
+            }
+
+        }
+        if (isPrime) {
+            prime = i;
+        }
+
+    }
+        return prime;
+  }
+
+
+
+}
+
--- spec/challenge_7-spec.js
+++ spec/challenge_7-spec.js
@@ -1,5 +1,5 @@
 // import challenge 7
-var LastPrime = require("../challenge_7").LastPrime;
+var PrimeFinder = require("../challenge_7").PrimeFinder;
 
 // describe the class PrimeFinder
 describe("The last prime number", function () {
@@ -7,13 +7,13 @@ describe("The last prime number", function () {
 // write your tests here
 
     it("for the first 10 natural numbers is XXX", function () {
-        var primeLast = new LastPrime(10);
-        expect( primeLast.lastPrimeNumber() ).toEqual(7);
+        var lastPrime = new PrimeFinder(10);
+        expect( lastPrime.getPrimeNumber() ).toEqual(7);
     });
 
     it("for the first 10,001 natural numbers is XXX", function () {
-        var primeLast = new LastPrime(10001);
-        expect( primeLast.lastPrimeNumber() ).toEqual(-1);
+        var lastPrime = new PrimeFinder(10001);
+        expect( lastPrime.getPrimeNumber() ).toEqual(9973);
     });
 
 });
